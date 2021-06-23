80eb8c66781447e022000736274161ba0054b03c
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-06-23 15:14:04 -0600

    models

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,45 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTERGER,
+      allowNUll: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+    },
+    stock: {
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      defaultValue: 10, 
+      validate: {
+        isNumeric: true,
+      }
+    },
+    catergory_id: {
+      type: DataTypes.INTERGER,
+      references: {
+        model: 'catergory',
+        key: 'id'
+      }
+
+    },
   },
   {
     sequelize,
