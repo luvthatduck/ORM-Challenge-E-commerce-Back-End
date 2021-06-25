7e28f5766adc81c4d4d0398faef5e9c8de6f837e
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-06-24 21:38:04 -0600

    got it working

--- models/Product.js
+++ models/Product.js
@@ -11,7 +11,7 @@ Product.init(
   {
     // define columns
     id: {
-      type: DataTypes.INTERGER,
+      type: DataTypes.INTEGER,
       allowNUll: false,
       primaryKey: true,
       autoIncrement: true
@@ -28,7 +28,7 @@ Product.init(
       }
     },
     stock: {
-      type: DataTypes.INTERGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10, 
       validate: {
@@ -36,7 +36,7 @@ Product.init(
       }
     },
     catergory_id: {
-      type: DataTypes.INTERGER,
+      type: DataTypes.INTEGER,
       references: {
         model: 'catergory',
         key: 'id'
